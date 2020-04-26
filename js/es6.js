4491231d93150ce03b8f3facedffee4589081998
whyjxh <1947200086@qq.com>
2020-04-26 23:13:35 +0800

    .

--- js/es6.js
+++ js/es6.js
@@ -39,3 +39,14 @@ function compose(...args) {
         }, val)
     }
 }
+/**
+ * var\let\const的区别
+ * var存在变量提升、let/const不存在；
+ * var全局定义附在window上、let/const不会；
+ * var在声明之前使用不会报错、let/const会报错，存在暂时性死区；
+ * 
+ * let 在定义时或者使用时赋值；
+ * const在定义时必须赋值；
+ * 
+ * let 可以改变/const 不可以改变（引用类型不可改变指向）；
+ */
