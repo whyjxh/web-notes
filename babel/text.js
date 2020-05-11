126eed89f346174a4ce9a62578cb0e2c709e67bd
Lawrencewang <lawrencewang@lagou.com>
2020-05-11 13:45:30 +0800

    添加babel

---
+++ babel/text.js
@@ -0,0 +1,21 @@
+/**
+ * babel 转译代码
+ */
+/**
+ * babel-loader
+ * @babel/core
+ * @babel/preset-env
+ * [
+ *    [
+ *        "@babel/preset-env",
+ *        {
+ *          "useBuiltIns": "usage",
+ *          "corejs": 3
+ *        }
+ *    ]
+ * ]
+ * @babel-polyfill
+ * 
+ * @babel/plugin-transform-runtime -D
+ * @babel/runtime -S
+ */
